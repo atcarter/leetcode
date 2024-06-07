ea4e3ccb11a0c2386abf0e4e00e11e9cc024173f
atcarter <atcarter005@gmail.com>
2024-06-07 02:55:09 -0500

    Add problem 167. Two Sum II

---
+++ spec/two-pointers/two_sum_ii_sorted_input_array_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'TwoSumIiSortedInputArray' do
+  context 'given a sorted input array and a target integer' do
+    it 'returns the 1-indexed indices of the two numbers that sum to target' do
+      numbers_one = [2, 7, 11, 15]
+      target_one = 9
+      numbers_two = [2, 3, 4]
+      target_two = 6
+      numbers_three = [-1, 0]
+      target_three = -1
+
+      expect(two_sum(numbers_one, target_one)).to eq([1, 2])
+      expect(two_sum(numbers_two, target_two)).to eq([1, 3])
+      expect(two_sum(numbers_three, target_three)).to eq([1, 2])
+    end
+  end
+end

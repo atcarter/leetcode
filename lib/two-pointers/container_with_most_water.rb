f9667710289a15cdfd628dc3d962b02f80c18dfe
atcarter <atcarter005@gmail.com>
2024-06-10 02:04:56 -0500

    Add problem 11. Container with Most Water

---
+++ lib/two-pointers/container_with_most_water.rb
@@ -0,0 +1,25 @@
+# @param {Integer[]} height
+# @return {Integer}
+# TC: O(n)
+# SC: O(1)
+def max_area(height)
+  max = 0
+  left = 0
+  right = height.length - 1
+
+  while left < right
+    area = (right - left) * [height[left], height[right]].min
+
+    if area > max
+      max = area
+    end
+
+    if height[left] <= height[right]
+      left += 1
+    else
+      right -= 1
+    end
+  end
+
+  max
+end
---
+++ spec/two-pointers/container_with_most_water_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe 'ContainerWithMostWater' do
+  context 'given height = [1,8,6,2,5,4,8,3,7]' do
+    it 'returns 49' do
+      height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+      expect(max_area(height)).to eq(49)
+    end
+  end
+
+  context 'given height = [1, 1]' do
+    it 'returns 1' do
+      height = [1, 1]
+
+      expect(max_area(height)).to eq(1)
+    end
+  end
+end
